26c86b8930eed2b615a043b60e954503bdffe63b
Tim <twilliams25@gmail.com>
2017-05-16 10:53:18 -0700

    drop cname and door updates

--- door/js/index.js
+++ door/js/index.js
@@ -1,3 +1,12 @@
+$('#doors').on('click', function() {
+  console.log("clicked the doors");
+  var leftThis = $('#left-door');
+  var rightThis = $('#right-door');
+  openDoorLeft(leftThis);
+  openDoorRight(rightThis);
+});
+
+
 function openDoorLeft(field) {
   var y = $(field).find(".thumb");
   var x = y.attr("class");
@@ -21,3 +30,38 @@ function openDoorRight(field) {
     y.addClass("thumbOpenedRight");
   }
 }
+
+var passMap = {
+  'investor' : 'about.html',
+  'buyer' : 'index.html',
+  'pass3' : 'location3',
+  'pass4' : 'location4'
+};
+
+$('#secret').on('keydown', function(event) {
+  if (event.keyCode == 13) {
+    var inputVal = $('#secret').val();
+    var destination = passMap[inputVal];
+
+    if (destination) {
+      // console.log("Got password " + inputVal + " sending to location: " + destination);
+      var leftThis = $('#left-door');
+      var rightThis = $('#right-door');
+      openDoorLeft(leftThis);
+      openDoorRight(rightThis);
+
+      setTimeout(function() {
+        // similar behavior as an HTTP redirect
+        // window.location.replace('../'+destination);
+
+        // similar behavior as clicking on a link
+        // window.location.href = '../'+destination;
+        $(location).attr('href', '../'+destination);
+      }, 1500);
+    } else {
+      //give them an error message? or maybe just link them to the index page.
+      console.log("No destination for that password");
+    }
+  }
+});
+
